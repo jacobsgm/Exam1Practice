211c70f496cf38281234c9248129fd872ecaf6cf
jacobsgm <jacobsgm@rose-hulman.edu>
2017-12-05 14:36:49 -0500

    ugh

--- src/problem1.py
+++ src/problem1.py
@@ -193,6 +193,34 @@ def run_test_problem1b():
     print('Testing the   problem1b   function:')
     print('--------------------------------------------------')
 
+    # Test 1:
+    expected = 5   # This is APPROXIMATELY the correct answer.
+    answer = problem1b(3, 5)
+    print()
+    print('Test 1 expected:', expected, '(approximately)')
+    print('       actual:  ', answer)
+
+    # Test 2:
+    expected = 1  # This is APPROXIMATELY the correct answer.
+    answer = problem1b(2, 1)
+    print()
+    print('Test 2 expected:', expected, '(approximately)')
+    print('       actual:  ', answer)
+
+    # Test 3:
+    expected = 44 # This is APPROXIMATELY the correct answer.
+    answer = problem1b(5, 40)
+    print()
+    print('Test 3 expected:', expected, '(approximately)')
+    print('       actual:  ', answer)
+
+    # Test 4:
+    expected = 1.9552  # This is APPROXIMATELY the correct answer.
+    answer = problem1b(0, 3)
+    print()
+    print('Test 4 expected:', expected, '(approximately)')
+    print('       actual:  ', answer)
+
 
 def problem1b(m, f):
     """
@@ -228,8 +256,15 @@ def problem1b(m, f):
     count = 0
     n = m*f
     for k in range(n-m+1):
-        if((k+m)% 2 == 1 & ((k+m) != 2)):
-            
+
+     #   if((k+m) == 2 & (k+m)% 2 == 0):
+     #       count = count+1
+        if((k+m) % 2 == 1 & ()):
+            count = count+1
+
+    return count
+
+
 
 
 
